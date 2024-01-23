8b5382f3bff40805d2ae1981e5fb48e96d293ccc
James <jameskimanzi@gmail.com>
2024-01-23 22:36:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+
+  const input = document.getElementById('input');
+
+  const clickAlert = () => {
+    alert('I was clicked!');
+  }
+  
+  input.addEventListener('click', clickAlert);
+
 }
+
+addingEventListener()
+
